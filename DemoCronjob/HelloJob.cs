245587ad4f18b23de9a7eeb8619cc85243da37f0
nta228 <64982003+nta228@users.noreply.github.com>
2022-03-19 08:17:52 +0700

    Add project files.

---
+++ DemoCronjob/HelloJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCronjob
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("Hello World");
+        }
+    }
+}
---
+++ DemoCronjob/Program.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCronjob
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            /*Console.OutputEncoding = Encoding.UTF8;
+            var source = "https://vnexpress.net/kinh-doanh";
+            var web = new HtmlWeb();
+            HtmlDocument doc = web.Load(source);
+            var nodeList = doc.QuerySelectorAll("h3.title-news a");
+            HashSet<string> setLink = new HashSet<string>();
+            foreach (var node in nodeList)
+            {
+                var link = node.Attributes["href"].Value;
+                if (string.IsNullOrEmpty(link))
+                {
+                    continue;
+                }
+                setLink.Add(link);
+                Console.WriteLine(link.ToString());
+            }*/
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+            scheduler.Start();
+            IJobDetail job = JobBuilder.Create<HelloJob>()
+                .WithIdentity("myjob", "group1")
+                .Build();
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("mytrigger", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(2)
+                    .RepeatForever())
+                .Build();
+            scheduler.ScheduleJob(job, trigger);
+            Console.ReadLine();
+        }
+    }
+}
